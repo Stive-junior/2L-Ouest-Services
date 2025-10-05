cb332dddc7b273846fbf4bf8ccb210ed0a20f81f
stive-junior <nanatchoffojunior@gmail.com>
2025-10-06 00:54:38 +0100

    regelement des messagerie SMTP via Mailsender

--- frontend/public/js/modules/utils.js
+++ frontend/public/js/modules/utils.js
@@ -22,8 +22,8 @@ let firebaseConfigCache = null; // Note : Initialisé à null, mais pourrait êt
 const CONFIG_CACHE_KEY = 'firebaseConfigCache';
 const CACHE_TTL = 24 * 60 * 60 * 1000;
 
-//export const API_BASE_URL = 'https://twol-ouest-services.onrender.com/api';
-export const  API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'https://twol-ouest-services.onrender.com/api';
+//export const  API_BASE_URL = 'http://localhost:8000/api';
 export const USER_CACHE_KEY = 'userDataCache';
 
 let isShowingErrorModal = false;
